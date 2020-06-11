4a21f8bf550badb4b6c03b65d67e124cc8f3eac0
sravan4990 <vangalasravanreddy@gmail.com>
2020-06-11 11:10:36 -0400

    Create abc_git.php

---
+++ abc_git.php
@@ -0,0 +1,26 @@
+public function abc($a,$b,$c){
+
+    $response = \Drupal::httpClient()->get('http://mywordpress.org/slotapi', array('headers' => array('Accept' => 'application/json')));
+      $json = (string) $response->getBody();
+      $data = json_decode($json);
+      $slots = [];
+      foreach ($data as $obj) {
+          if($obj->field_category === $a
+            && $obj->field_location === $b && $obj->field_date === $c && $obj->field_capacity != 0) {
+            $gg = explode("-",$obj->field_hours);
+            $fi = date("g:i a", strtotime($gg[0])) . '-' . date("g:i a", strtotime($gg[1]));
+            $slots[$obj->field_hours] = $fi ; 
+
+          } else {
+            //var_dump('No Available Slots');
+          }
+       }    
+
+       if(!empty($slots)){
+        return $slots;
+       }
+       else{
+        $slots = ['a','b','c'];
+        return $slots;
+       }
+    }
