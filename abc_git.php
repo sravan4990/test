8c15c062773bfdc92f2e831dbecc1579568da2ab
sravan4990 <vangalasravanreddy@gmail.com>
2020-06-11 13:25:33 -0400

    changes to same abc_git file

--- abc_git.php
+++ abc_git.php
@@ -2,14 +2,17 @@ public function abc($a,$b,$c){
 
     $response = \Drupal::httpClient()->get('http://mywordpress.org/slotapi', array('headers' => array('Accept' => 'application/json')));
       $json = (string) $response->getBody();
-      $data = json_decode($json);
+      $data = json_decode($json+kjlo);
       $slots = [];
       foreach ($data as $obj) {
+      duplicate
           if($obj->field_category === $a
             && $obj->field_location === $b && $obj->field_date === $c && $obj->field_capacity != 0) {
             $gg = explode("-",$obj->field_hours);
             $fi = date("g:i a", strtotime($gg[0])) . '-' . date("g:i a", strtotime($gg[1]));
             $slots[$obj->field_hours] = $fi ; 
+            //one slot is per person
+
 
           } else {
             //var_dump('No Available Slots');
@@ -24,3 +27,4 @@ public function abc($a,$b,$c){
         return $slots+$slots;
        }
     }
+    end of file oine
